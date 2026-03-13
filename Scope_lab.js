f5b7e9f2f0d13445820c2a0bfd5ca9283357675e
Arumansan <a.kaviani118@gmail.com>
2026-03-13 23:35:29 +0100

    completed practice task

--- Scope_lab.js
+++ Scope_lab.js
@@ -24,3 +24,19 @@ show();
 // console.log(functionVar); // Throws ReferenceError
 // console.log(functionLet); // Throws ReferenceError
 // console.log(functionConst); // Throws ReferenceError
+// --- Practice Task ---
+{
+    var myVar = "Original Var";
+    let myLet = "Original Let";
+    const myConst = "Original Const";
+
+    // تلاش برای تغییر مقادیر داخل بلاک
+    myVar = "New Var inside";
+    myLet = "New Let inside";
+    // myConst = "New Const inside"; // اینجا رو دقت کنید!
+}
+
+// تلاش برای تغییر مقادیر بیرون بلاک
+myVar = "New Var outside";
+myLet = "New Let outside"; // اینجا رو هم دقت کنید!
+console.log(mylet);
